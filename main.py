224d8233d67ddf9861ed9e10b0f496010ed4db4e
DaniMatul <mayda.matul@gmail.com>
2023-02-14 09:52:14 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+
+
 print("Nombre: Mayda Daniela Matul Alvarado, Carné: 1535523")
+
+import os 
+
+mi ubicacion = os.getcwd()
+if os.path.exists("modulos"):
+    print("La carpeta ya existe")
+else: 
+    os.mkdir(mi ubicacion + "\\modulos")
+    archivo = open('/modulos/prueba.txt', 'w')
+    archivo.write('Hola mundo')
+    archivo-close()
